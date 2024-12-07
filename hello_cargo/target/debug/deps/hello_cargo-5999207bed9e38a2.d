8ce74a9de2dece0ce5b8999969788a22c4d59f43
tingachris <chrispinetinega@gmail.com>
2024-12-07 14:07:10 +0300

    Initialize Hello Cargo project with basic structure and main function

---
+++ hello_cargo/target/debug/deps/hello_cargo-5999207bed9e38a2.d
@@ -0,0 +1,5 @@
+D:\rust-projects\hello_cargo\target\debug\deps\libhello_cargo-5999207bed9e38a2.rmeta: src/main.rs
+
+D:\rust-projects\hello_cargo\target\debug\deps\hello_cargo-5999207bed9e38a2.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\rust-projects\hello_cargo\target\debug\hello_cargo.exe: D:\rust-projects\hello_cargo\src\main.rs
