93af45a6295341859ec35ed82faef7583a09379d
tingachris <chrispinetinega@gmail.com>
2025-01-10 22:55:39 +0300

    Refactor read_tcp_data function to use SocketAddr and improve error handling

--- read_write_modbus_tcp_rtu/src/modbus/tcp.rs
+++ read_write_modbus_tcp_rtu/src/modbus/tcp.rs
@@ -1,7 +1,10 @@
+use std::net::SocketAddr;
+use std::error::Error;
+
 use tokio_modbus::prelude::*;
 
-pub async fn read_tcp_data(ip: &str, port: u16, slave: u8, register: u16) -> Result<u16, Box<dyn std::error::Error>> {
-    let socket_addr = format!("{}:{}", ip, port).parse()?;
+pub async fn read_tcp_data(ip: &str, port: u16, slave: u8, register: u16) -> Result<u16, Box<dyn Error>> {
+    let socket_addr: SocketAddr = format!("{}:{}", ip, port).parse()?;
     let mut ctx = tcp::connect_slave(socket_addr, Slave(slave)).await?;
     let response = ctx.read_holding_registers(register, 1).await?;
     ctx.disconnect().await?;
