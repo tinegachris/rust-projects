d219b3cd3d12b3db8f0e7adbb30895563e147941
tingachris <chrispinetinega@gmail.com>
2025-01-10 17:34:09 +0300

    Refactor main function to use unified read_data method for RTU and TCP communication

--- read_write_modbus_tcp_rtu/src/main.rs
+++ read_write_modbus_tcp_rtu/src/main.rs
@@ -1,18 +1,15 @@
-mod modbus;
-
-use modbus::rtu::read_rtu_data;
-use modbus::tcp::read_tcp_data;
+use modbus::{read_data, ModbusProtocol};
 
 #[tokio::main]
 async fn main() {
     // Example for RTU Communication
-    match read_rtu_data("/dev/ttyUSB0", 1, 100).await {
+    match read_data(ModbusProtocol::RTU, "/dev/ttyUSB0", 1, 100).await {
         Ok(value) => println!("RTU: Register value is {}", value),
         Err(err) => eprintln!("RTU Error: {}", err),
     }
 
     // Example for TCP Communication
-    match read_tcp_data("192.168.1.100", 502, 1, 100).await {
+    match read_data(ModbusProtocol::TCP, "192.168.1.100:502", 1, 100).await {
         Ok(value) => println!("TCP: Register value is {}", value),
         Err(err) => eprintln!("TCP Error: {}", err),
     }
